efee6f717993dc274d40a964ddb286d01e5f33c7
Natal Vande Casteele <natal@libius.be>
2021-06-16 09:04:57 +0200

    laatste nieuwe ng-kaart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClassicModule, KaartModule } from '@wegenenverkeer/ng-kaart';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    KaartModule.withDefaults(),
+    ClassicModule,
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
